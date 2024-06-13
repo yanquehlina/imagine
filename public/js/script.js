fbdabe9526de809be9a103ce87c78c6fce93f85b
Maame <c.maameyankeh@gmail.com>
2024-06-13 12:20:23 +0000

    Classes in JavaScript

--- public/js/script.js
+++ public/js/script.js
@@ -281,4 +281,45 @@ function birthdayGreeting (user) {
 birthdayGreeting(user)
 
 
+// Defining Object
+// Bottle Class
+class Bottle {
+  constructor(color, shape, volume) {
+    this.color = color;
+    this.shape = shape;
+    this.volume = volume;
+    
+  }
+  // putting object methods in class element
+  getInfo() {
+    return `This is a ${this.color} ${this.shape} bottle with a capacity of ${this.volume}ml`;
+  }
+}
+
+class Car {}
+
+const bottle1 = new Bottle('black', 'circle', 300);
+bottle1.getInfo();
+const bottle2 = new Bottle('yellow', 'square', 100);
+bottle2.getInfo();
+
+bottle1 instanceof Car
+
+// Object Methods
+const bottle = {
+  color: `red`,
+  shape: `square`,
+  volume: `100`,
+  getInfo: function() {
+    return `This is a ${this.color} ${this.shape} bottle with a capacity of ${this.volume}ml`;
+  },
+}
+
+bottle.color;
+bottle.getInfo();
+// notes
+// anonymous function: a function without a name 
+    // getInfo: function()
+// element class items start with capital ;etters defying the Carmel case of JavaScript
+// JavaScript class is a template used to duplicate objects multiple times
 
